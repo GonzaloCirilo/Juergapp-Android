26c8ef601a4bcedb2378d9dea30a9bdaecfde64c
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-04-18 19:58:31 -0500

    Added Event Model and Core Module Dependency

---
+++ core/src/main/java/pe/com/redcups/core/model/Event.kt
@@ -0,0 +1,5 @@
+package pe.com.redcups.core.model
+
+import java.util.Date
+
+data class Event(var name: String, var user: Int, var date: Date = Date(), var latitude: Double = 0.0, var longitude: Double = 0.0, var address: String = "", var description: String = "")
