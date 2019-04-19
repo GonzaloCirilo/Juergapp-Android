ba3f2b3c799259c61dd809cfeb633e1961bf8a58
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-04-18 19:58:52 -0500

    Basic Events Recycler View

---
+++ app/src/main/java/pe/com/redcups/juergapp_android/adapter/EventAdapter.kt
@@ -0,0 +1,34 @@
+package pe.com.redcups.juergapp_android.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recycler_view_event.view.*
+import pe.com.redcups.core.model.Event
+import pe.com.redcups.juergapp_android.R
+
+class EventAdapter(private val events: ArrayList<Event>, context: Context): RecyclerView.Adapter<EventAdapter.ViewHolder>(){
+
+    private var inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = inflater.inflate(R.layout.recycler_view_event, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.eventTextView.text = events[position].name
+        holder.eventImageView.setImageResource(R.mipmap.event_image_placeholder)
+    }
+
+    inner class ViewHolder(eventView: View): RecyclerView.ViewHolder(eventView) {
+        val eventTextView: TextView = eventView.event_name
+        val eventImageView: ImageView = eventView.event_image
+    }
+}
--- app/src/main/java/pe/com/redcups/juergapp_android/fragment/EventFragment.kt
+++ app/src/main/java/pe/com/redcups/juergapp_android/fragment/EventFragment.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_event.*
+import pe.com.redcups.core.model.Event
 
 import pe.com.redcups.juergapp_android.R
+import pe.com.redcups.juergapp_android.adapter.EventAdapter
 
 class EventFragment : Fragment() {
+
+    private var events = ArrayList<Event>()
+    private lateinit var eventAdapter: EventAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        for (i in 1..10){
+            events.add(Event("Event $i", i))
+        }
         return inflater.inflate(R.layout.fragment_event, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        eventAdapter = EventAdapter(events,view.context)
+        recycler_view_event.apply{
+            adapter = eventAdapter
+            layoutManager = LinearLayoutManager(view.context)
+        }
+    }
+
 
 }
