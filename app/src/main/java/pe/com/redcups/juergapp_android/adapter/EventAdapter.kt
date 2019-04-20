2d2555693a4437d6ecb06c428846f1ba87898497
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-04-20 15:45:07 -0500

    Add Event Detail

--- app/src/main/java/pe/com/redcups/juergapp_android/adapter/EventAdapter.kt
+++ app/src/main/java/pe/com/redcups/juergapp_android/adapter/EventAdapter.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recycler_view_event.view.*
 import pe.com.redcups.core.model.Event
 import pe.com.redcups.juergapp_android.R
+import pe.com.redcups.juergapp_android.fragment.EventDetailFragment
 
 class EventAdapter(private val events: ArrayList<Event>, context: Context): RecyclerView.Adapter<EventAdapter.ViewHolder>(){
 
@@ -25,10 +28,28 @@ class EventAdapter(private val events: ArrayList<Event>, context: Context): Recy
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.eventTextView.text = events[position].name
         holder.eventImageView.setImageResource(R.mipmap.event_image_placeholder)
+
     }
 
-    inner class ViewHolder(eventView: View): RecyclerView.ViewHolder(eventView) {
+    inner class ViewHolder(eventView: View): RecyclerView.ViewHolder(eventView), View.OnClickListener {
         val eventTextView: TextView = eventView.event_name
         val eventImageView: ImageView = eventView.event_image
+
+        init {
+            with(itemView){
+                setOnClickListener(this@ViewHolder)
+            }
+        }
+
+        override fun onClick(v: View?) {
+            // TODO pass Event object as bundle or consider using a ViewModel
+            val fragment = EventDetailFragment()
+            (v?.context as AppCompatActivity)
+                .supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container,fragment,fragment.javaClass.name)
+                .addToBackStack("EventStack")
+                .commit()
+        }
     }
 }
--- core/src/main/java/pe/com/redcups/core/model/Event.kt
+++ core/src/main/java/pe/com/redcups/core/model/Event.kt
@@ -2,4 +2,11 @@ package pe.com.redcups.core.model
 
 import java.util.Date
 
-data class Event(var name: String, var user: Int, var date: Date = Date(), var latitude: Double = 0.0, var longitude: Double = 0.0, var address: String = "", var description: String = "")
+data class Event(var id: Int = 0,
+                 var name: String,
+                 var user: Int,
+                 var date: Date = Date(),
+                 var latitude: Double = 0.0,
+                 var longitude: Double = 0.0,
+                 var address: String = "",
+                 var description: String = "")
