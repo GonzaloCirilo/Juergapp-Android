fe064195a4e28b3f124ef0e5424e722261e4a933
Franco Rivera <franco@riverarivas.com>
2019-05-02 12:52:01 -0500

    add product category model, networking and test

---
+++ core/src/main/java/pe/com/redcups/core/model/ProductCategory.kt
@@ -0,0 +1,8 @@
+package pe.com.redcups.core.model
+
+data class ProductCategory (
+    var id: Int = 0,
+    var name: String = "Nombre de la Categoría",
+    var picture: String = "URL de la imagen",
+    var description: String = "Breve descripción de Categoría"
+)
--- core/src/main/java/pe/com/redcups/core/network/Constants.kt
+++ core/src/main/java/pe/com/redcups/core/network/Constants.kt
@@ -1,10 +1,23 @@
 package pe.com.redcups.core.network
 
 import pe.com.redcups.core.model.Event
+import pe.com.redcups.core.model.ProductCategory
 
 object Constants{
     private const val baseURL = "https://wemake.pe/juergapp/api/v1"
+
+    // Endpoitns
+
+    // Events
     var eventsURL = "$baseURL/events"
 
-    val map: Map<Class<*>,String> = mapOf(Array<Event>::class.java to eventsURL)
+
+    // Products
+    var product_categoriesURL = "$baseURL/product_categories"
+
+    // Mapping endpoints to Classes
+
+    val map: Map<Class<*>,String> = mapOf(Array<Event>::class.java to eventsURL,
+                                                Array<ProductCategory>::class.java to product_categoriesURL
+                                                )
 }
