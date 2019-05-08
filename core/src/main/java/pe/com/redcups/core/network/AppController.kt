e20e1820e752b2269b6b0ca8d14b3e8cc12d1786
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-05-07 19:26:28 -0500

    JuergappAPI interface refactored

--- core/src/main/java/pe/com/redcups/core/network/AppController.kt
+++ core/src/main/java/pe/com/redcups/core/network/AppController.kt
@@ -1,35 +1,57 @@
 package pe.com.redcups.core.network
 
-import android.app.Application
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.LruCache
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageLoader
 import com.android.volley.toolbox.Volley
+import org.jetbrains.annotations.TestOnly
 import java.util.*
 
-class AppController: Application() {
+class AppController constructor(context: Context) {
 
-    override fun onCreate() {
-        super.onCreate()
-        mInstance = this
+    private lateinit var requestQueue: RequestQueue
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppController? = null
+
+        fun getInstance(context: Context) =
+                INSTANCE ?: synchronized(this) {
+                    INSTANCE ?: AppController(context).also {
+                        INSTANCE = it
+                        INSTANCE!!.initRequestQueue(context)
+
+                    }
+                }
+        fun getInstance() = INSTANCE!!
     }
 
-    private fun getRequestQueue(): RequestQueue = requestQueue ?: initRequestQueue(Volley.newRequestQueue(applicationContext))
+    val imageLoader: ImageLoader by lazy {
+        ImageLoader(requestQueue,
+            object : ImageLoader.ImageCache {
+                private val cache = LruCache<String, Bitmap>(20)
+                override fun getBitmap(url: String): Bitmap {
+                    return cache.get(url)
+                }
+                override fun putBitmap(url: String, bitmap: Bitmap) {
+                    cache.put(url, bitmap)
+                }
+            })
+    }
 
-    fun <T> addRequest(request: Request<T>) = getRequestQueue().add(request)
+    fun <T> addRequest(request: Request<T>) = requestQueue.add(request)
 
     fun cancelRequestWithTag(tag: Objects) = requestQueue?.let { requestQueue!!.cancelAll(tag) }
 
-    companion object{
+    private fun initRequestQueue(context: Context){
+        requestQueue= Volley.newRequestQueue(context)
+    }
 
-        private var mInstance: AppController = AppController()
-
-        private var requestQueue: RequestQueue? = null
-
-        fun getInstance(): AppController = mInstance
-
-        fun initRequestQueue(requestQueue: RequestQueue): RequestQueue {
-            this.requestQueue = requestQueue
-            return this.requestQueue!!
-        }
+    @TestOnly
+    fun setRequestQueue(requestQueue: RequestQueue){
+        INSTANCE!!.requestQueue = requestQueue
     }
 }
--- core/src/main/java/pe/com/redcups/core/network/JuergappAPI.kt
+++ core/src/main/java/pe/com/redcups/core/network/JuergappAPI.kt
@@ -1,38 +1,58 @@
 package pe.com.redcups.core.network
 
+import android.content.Context
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
+import org.jetbrains.annotations.TestOnly
 
-class JuergappAPI {
+class JuergappAPI constructor(flag: Context) {
 
-    companion object{
-        private fun <T> buildRequest(clazz: Class<T>, method: Int, response: (T) -> Unit, errorResponse: (VolleyError)-> Unit, body: T? = null){
-            val request = GsonRequest(
-                Constants.map[clazz] ?: "",
-                clazz,
-                method,
-                Response.Listener{
-                    response(it)
-                },
-                Response.ErrorListener{
-                    errorResponse(it)
-                },
-                body
-            )
-            AppController.getInstance().addRequest(request)
-        }
+    companion object {
+        @Volatile
+        private var INSTANCE: JuergappAPI? = null
 
-        fun <T> getResource(clazz: Class<T>, response: (T) -> Unit, errorResponse: (VolleyError)-> Unit){
-            buildRequest(clazz, Request.Method.GET, response, errorResponse)
-        }
-
-        fun <T> postResource(clazz: Class<T>, response: (T) -> Unit, errorResponse: (VolleyError) -> Unit, body: T){
-            buildRequest(clazz, Request.Method.POST, response, errorResponse,body)
-        }
-
-        fun <T> deleteResurce(clazz: Class<T>, response: (T) -> Unit, errorResponse: (VolleyError)-> Unit){
-            buildRequest(clazz, Request.Method.DELETE, response, errorResponse)
-        }
+        fun getInstance(ctx: Context) =
+                INSTANCE ?: synchronized(this) {
+                    INSTANCE ?: JuergappAPI(ctx).also{
+                        INSTANCE = it
+                    }
+                }
     }
+
+    private fun <T> buildRequest(
+        clazz: Class<T>,
+        method: Int,
+        response: (T) -> Unit,
+        errorResponse: (VolleyError) -> Unit,
+        body: T? = null
+    ) {
+        val request = GsonRequest(
+            Constants.map[clazz] ?: "",
+            clazz,
+            method,
+            Response.Listener {
+                response(it)
+            },
+            Response.ErrorListener {
+                errorResponse(it)
+            },
+            body
+        )
+        AppController.getInstance().addRequest(request)
+    }
+
+    fun <T> getResource(clazz: Class<T>, response: (T) -> Unit, errorResponse: (VolleyError) -> Unit) {
+        buildRequest(clazz, Request.Method.GET, response, errorResponse)
+    }
+
+    fun <T> postResource(clazz: Class<T>, response: (T) -> Unit, errorResponse: (VolleyError) -> Unit, body: T) {
+        buildRequest(clazz, Request.Method.POST, response, errorResponse, body)
+    }
+
+    fun <T> deleteResurce(clazz: Class<T>, response: (T) -> Unit, errorResponse: (VolleyError) -> Unit) {
+        buildRequest(clazz, Request.Method.DELETE, response, errorResponse)
+    }
+
 }
