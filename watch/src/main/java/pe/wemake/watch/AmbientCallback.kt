91654e772a8851d2677b46723b27fe35514e3733
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-06-14 00:31:53 -0500

    Add basic watch view

--- watch/src/main/java/pe/wemake/watch/AmbientCallback.kt
+++ watch/src/main/java/pe/wemake/watch/AmbientCallback.kt
@@ -1,2 +1,19 @@
 package pe.wemake.watch
 
+import android.os.Bundle
+import androidx.wear.ambient.AmbientModeSupport
+
+class AmbientCallback: AmbientModeSupport.AmbientCallback() {
+    override fun onEnterAmbient(ambientDetails: Bundle?) {
+        // Handle entering ambient mode
+    }
+
+    override fun onExitAmbient() {
+        // Handle exiting ambient mode
+    }
+
+    override fun onUpdateAmbient() {
+        // Update the content
+    }
+
+}
--- watch/src/main/java/pe/wemake/watch/MainActivity.kt
+++ watch/src/main/java/pe/wemake/watch/MainActivity.kt
@@ -1,15 +1,44 @@
 package pe.wemake.watch
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.wear.ambient.AmbientModeSupport
+import kotlinx.android.synthetic.main.activity_main.*
+import pe.com.redcups.core.utilities.InjectorUtils
+import pe.com.redcups.core.viewmodel.events.EventViewModel
 
-class MainActivity : WearableActivity() {
+class MainActivity : FragmentActivity(), AmbientModeSupport.AmbientCallbackProvider {
+
+
+    override fun getAmbientCallback(): AmbientModeSupport.AmbientCallback = AmbientCallback()
+
+    private lateinit var adapter: EventAdapter
+    private lateinit var ambientController: AmbientModeSupport.AmbientController
+
+
+    private val viewModel: EventViewModel by viewModels{
+        InjectorUtils.provideEventViewModelFactory(applicationContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Enables Always-on
-        setAmbientEnabled()
+        ambientController = AmbientModeSupport.attach(this)
+
+        adapter = EventAdapter(applicationContext)
+
+        wearable_recycler_view.adapter = adapter
+        wearable_recycler_view.layoutManager= LinearLayoutManager(applicationContext)
+
+        viewModel.events.observe(this, Observer {
+            adapter.setEvents(it)
+        })
     }
+
 }
