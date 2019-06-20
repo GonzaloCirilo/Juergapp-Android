ba755ff17adf55fcd5aafcd0a6cf0af153b6208c
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-06-20 13:57:57 -0500

    Update Models

--- core/src/main/java/pe/com/redcups/core/model/Supplier.kt
+++ core/src/main/java/pe/com/redcups/core/model/Supplier.kt
@@ -1,7 +1,9 @@
 package pe.com.redcups.core.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "supplier_table")
 data class Supplier(
     @PrimaryKey var id: Int,
     var ruc: String,
