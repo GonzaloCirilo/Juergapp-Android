335212946fc72e3dd29c2c0319aeb288f9850dc7
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-06-20 14:12:31 -0500

    Add layout for fragments

--- app/src/main/java/pe/com/redcups/juergapp_android/MainActivity.kt
+++ app/src/main/java/pe/com/redcups/juergapp_android/MainActivity.kt
@@ -2,13 +2,12 @@ package pe.com.redcups.juergapp_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -62,4 +61,15 @@ class MainActivity : AppCompatActivity() {
         // show the up arrow or drawer menu icon
         setupActionBarWithNavController(navController, appBarConfig)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.cart_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
+
 }
