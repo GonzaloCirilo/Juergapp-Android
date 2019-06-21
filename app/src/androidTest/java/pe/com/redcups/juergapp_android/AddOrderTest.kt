62ed4a0eefe0b700068025f078a66908bf1c1c4a
Franco Rivera <franco@riverarivas.com>
2019-06-21 16:20:29 -0500

    add buy test

--- app/src/androidTest/java/pe/com/redcups/juergapp_android/AddOrderTest.kt
+++ app/src/androidTest/java/pe/com/redcups/juergapp_android/AddOrderTest.kt
@@ -4,7 +4,9 @@ import androidx.fragment.app.testing.launchFragment
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
@@ -18,6 +20,9 @@ import org.junit.Rule
 import org.junit.Test
 import pe.com.redcups.juergapp_android.fragment.EventAddFragment
 import pe.com.redcups.juergapp_android.adapter.EventAdapter
+import pe.com.redcups.juergapp_android.adapter.ProductCategoryAdapter
+import pe.com.redcups.juergapp_android.adapter.ProductListAdapter
+import java.util.*
 
 
 class AddOrderTest{
@@ -28,36 +33,69 @@ class AddOrderTest{
 
 
     @Test
-    fun changeText_sameActivity() {
+    fun addEvent() {
 
 
-        Espresso.onView(ViewMatchers.withId(R.id.events_dest)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.event_add_button)).perform(ViewActions.click())
+        // NAVIGATE - Setup
+        onView(withId(R.id.events_dest)).perform(ViewActions.click())
+        onView(withId(R.id.event_add_button)).perform(ViewActions.click())
 
-        // Delete whatevers on the test
-        Espresso.onView(ViewMatchers.withId(R.id.event_name_text_edit)).perform(ViewActions.clearText())
+        // NAME
 
-        // Type text and then press the button.
-        Espresso.onView(ViewMatchers.withId(R.id.event_name_text_edit))
-            .perform(ViewActions.typeText(STRING_TO_BE_TYPED), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.event_name_text_edit)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(NAME_TO_BE_TYPED),
+            ViewActions.closeSoftKeyboard())
 
+        // DESCRIPTION
 
-        Espresso.onView(ViewMatchers.withId(R.id.add_event_button)).perform(ViewActions.click())
+        onView(withId(R.id.event_description_text_edit)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(DESCRIPTION_TO_BE_TYPED),
+            ViewActions.closeSoftKeyboard())
 
-        Espresso.onView(ViewMatchers.withId(R.id.recycler_view_event))
-            //.check(ViewAssertions.matches(ViewMatchers.withText()))
-        // Check that the text was changed.
-        onView(withText(STRING_TO_BE_TYPED)).check(matches(isDisplayed()))
+        // ADDRESS
+        onView(withId(R.id.event_address_text_edit)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(ADDRESS_TO_BE_TYPED),
+            ViewActions.closeSoftKeyboard())
+
+        // Save new Event
+
+        onView(withId(R.id.add_event_button)).perform(ViewActions.click())
+
+        // TODO: Change this to actual find
+        for (i in 1..15){
+            onView(withId(R.id.recycler_view_event)).perform(ViewActions.swipeUp())
+        }
+
+        onView(withText(NAME_TO_BE_TYPED)).check(matches(isDisplayed()))
+        onView(withText(DESCRIPTION_TO_BE_TYPED)).check(matches(isDisplayed()))
+        onView(withText(ADDRESS_TO_BE_TYPED)).check(matches(isDisplayed()))
+
     }
 
     @Test
-    fun changeText_newActivity() {
-        // Type text and then press the button.
+    fun buyProduct() {
+
+        // NAVIGATE - Setup
+        onView(withId(R.id.product_category_dest)).perform(ViewActions.click())
+
+        onView(withId(R.id.recycler_view_product_category)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ProductCategoryAdapter.ViewHolder>(0, ViewActions.click()))
+        onView(withId(R.id.recycler_view_product_list)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ProductListAdapter.ViewHolder>(0, ViewActions.click()))
+        onView(withId(R.id.add_to_cart_button)).perform(ViewActions.click())
+        onView(withId(R.id.add_to_cart_button)).perform(ViewActions.click())
+
+
+
         }
 
-
     companion object {
+        val NAME_TO_BE_TYPED = "Event Espresso" + Date()
+        val DESCRIPTION_TO_BE_TYPED = "Description Espresso" + Date()
+        val ADDRESS_TO_BE_TYPED = "Address Espresso" + Date()
 
-        val STRING_TO_BE_TYPED = "Espresso view"
     }
 }
