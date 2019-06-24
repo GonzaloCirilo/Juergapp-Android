7045e1209429a8cd091dc795e7dc1bc3cc52a522
Franco Rivera <franco@riverarivas.com>
2019-06-24 12:14:00 -0500

    improve watch ui

---
+++ watch/src/main/java/pe/wemake/watch/NextEventActivity.kt
@@ -0,0 +1,13 @@
+package pe.wemake.watch
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class NextEventActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_event)
+    }
+}
